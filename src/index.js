e969607ae659869ad908553d9905a0fbf91413ea
chin-m1 <znuttawot.s@pttdigital.com>
2025-08-10 18:19:26 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,95 +1,58 @@
-const express = require('express');
-const cors = require('cors'); // Import CORS package
+const express = require("express");
+const cors = require("cors"); // Import CORS package
 const app = express();
 const port = 3007;
-const cron = require('node-cron');
-const axios = require('axios');
+const cron = require("node-cron");
+const axios = require("axios");
 
 // Import routes
-const userRoutes = require('./router/users');
-const historyRoutes = require('./router/histories');
-const recordRoutes = require('./router/records');
-const adminRoutes = require('./router/admins');
-const roleRoutes = require('./router/roles');
-const appRoutes = require('./router/app');
+const userRoutes = require("./router/users");
+const historyRoutes = require("./router/histories");
+const recordRoutes = require("./router/records");
+const adminRoutes = require("./router/admins");
+const roleRoutes = require("./router/roles");
+const appRoutes = require("./router/app");
 
 // Import database and other configs
-const { connectDB } = require('./config/db');
-const { fetchCurrentGameData, fetchNBAData } = require('./config/service/nba-service');
+const { connectDB } = require("./config/db");
+const {
+  fetchCurrentGameData,
+  fetchNBAData,
+} = require("./config/service/nba-service");
 
 // Connect to the database
 connectDB();
 
 // Middleware (optional: for parsing JSON or static files)
-app.use(cors({
-  origin: ['https://backoffice-nba.cquiz.app', 'http://localhost:3000'], // Allow both production and local development
-}));
+app.use(
+  cors({
+    origin: [
+      "http://ygevo.myvnc.com",
+      "https://ygevo.myvnc.com",
+      "http://localhost:3000",
+    ], // Allow both production and local development
+  })
+);
 app.use(express.json());
 
 // Use the routes
-app.use('/user', userRoutes);
-app.use('/history', historyRoutes);
-app.use('/record', recordRoutes);
-app.use('/admin', adminRoutes);
-app.use('/role', roleRoutes);
-app.use('/v1/app', appRoutes)
+app.use("/wnba/api/user", userRoutes);
+app.use("/wnba/api/history", historyRoutes);
+app.use("/wnba/api/record", recordRoutes);
+app.use("/wnba/api/admin", adminRoutes);
+app.use("/wnba/api/role", roleRoutes);
+app.use("/wnba/api/v1/app", appRoutes);
 
-cron.schedule('0 * * * *', async () => {
+cron.schedule("0 * * * *", async () => {
   try {
-    console.log('Running Cron Job at', new Date().toLocaleString());
-    const response = await axios.get(`http://27.254.145.186:3006/v1/app/nba`);
-
-    // Optional: Use the response data in your Discord message
-    const message = {
-      content: `🕐 [WNBA]Cron Job ran at ${new Date().toLocaleString()}.\nNBA data nba status: ${response.status}`
-    };
-
-    // Send message to Discord via webhook
-    await axios.post(
-      'https://discord.com/api/webhooks/1375409779249516554/7DefABFVzS4YQ9Rfg7-bdcOW8ElddRO9FgmwYix6O4YAFD8DlBuJ8o0XbykRN8Pa-Rru',
-      message
-    );
-  } catch (error) {
-    // Optional: Use the response data in your Discord message
-    const message = {
-      content: `🕐 [RETRY-WNBA]Cron Job ran at ${new Date().toLocaleString()}.\nNBA data retry-nba status: ${JSON.stringify(error)}`
-    };
-
-    // Send message to Discord via webhook
-    await axios.post(
-      'https://discord.com/api/webhooks/1375409779249516554/7DefABFVzS4YQ9Rfg7-bdcOW8ElddRO9FgmwYix6O4YAFD8DlBuJ8o0XbykRN8Pa-Rru',
-      message
-    );
-  }
+    await axios.get(`http://ygevo.myvnc.com/nba/api/v1/app/nba`);
+  } catch (error) {}
 });
 
-cron.schedule('0 * * * *', async () => {
+cron.schedule("0 * * * *", async () => {
   try {
-    console.log('Running Cron Job at', new Date().toLocaleString());
-    const response = await axios.get(`http://27.254.145.186:3006/v1/app/retry-nba`);
-
-    // Optional: Use the response data in your Discord message
-    const message = {
-      content: `🕐 [RETRY-WNBA]Cron Job ran at ${new Date().toLocaleString()}.\nNBA data retry-nba status: ${response.status}`
-    };
-
-    // Send message to Discord via webhook
-    await axios.post(
-      'https://discord.com/api/webhooks/1375409779249516554/7DefABFVzS4YQ9Rfg7-bdcOW8ElddRO9FgmwYix6O4YAFD8DlBuJ8o0XbykRN8Pa-Rru',
-      message
-    );
-  } catch (error) {
-    // Optional: Use the response data in your Discord message
-    const message = {
-      content: `🕐 [RETRY-WNBA]Cron Job ran at ${new Date().toLocaleString()}.\nNBA data retry-nba status: ${JSON.stringify(error)}`
-    };
-
-    // Send message to Discord via webhook
-    await axios.post(
-      'https://discord.com/api/webhooks/1375409779249516554/7DefABFVzS4YQ9Rfg7-bdcOW8ElddRO9FgmwYix6O4YAFD8DlBuJ8o0XbykRN8Pa-Rru',
-      message
-    );
-  }
+    await axios.get(`http://ygevo.myvnc.com/nba/api/v1/app/retry-nba`);
+  } catch (error) {}
 });
 
 // Start the server
